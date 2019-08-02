61e3bcd2372e7386f6db17da988867917596ab9b
ilhamksyuriadi <ilhamksyuriadi@gmail.com>
2019-08-03 01:28:40 +0700

    -

--- index.js
+++ index.js
@@ -57,6 +57,7 @@ app.use(function(req, res, next) {
 app.use('/', require('./routes/index.js'));
 app.use('/users', require('./routes/users.js'));
 
-const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server started on port ${PORT}`));
+app.listen(process.env.PORT || 3000, () => {
+  console.log("server is running on 3000 port...");
+});
