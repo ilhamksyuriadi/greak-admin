2ae3352e68dfbf4dbd1365baef3a12fbef4f69aa
ilhamksyuriadi <ilhamksyuriadi@gmail.com>
2019-08-04 16:58:43 +0700

    register page now can add data

--- index.js
+++ index.js
@@ -16,6 +16,14 @@ mongoose
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
 
+//Schema
+let userSchema = new mongoose.Schema({
+  id : String,
+  name : String,
+  password : String
+});
+let User = mongoose.model("User", userSchema)
+
 //EJS
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -26,12 +34,22 @@ app.use(express.urlencoded({ extended: true }));
 // Set static path
 app.use(express.static(path.join(__dirname, "public")));
 
+// Route
 app.get("/", (req, res) => {
     res.render("index");
 });
 
 app.get("/register", (req,res)=>{
   res.render("register")
+})
+
+app.post("/addUser", (req,res)=>{
+  let user = new User();
+  user.id = req.body.id;
+  user.name = req.body.name;
+  user.password = req.body.password;
+  user.save();
+  res.redirect("/")
 })
 
 app.listen(process.env.PORT || 3000, () => {
